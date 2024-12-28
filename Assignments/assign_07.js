59213e58967e29d7f21740cebacc72e04d47625b
Vikash Kumawat <vvikash0012@gmail.com>
2024-12-28 23:35:31 +0530

    Assignment

--- Assignments/assign_07.js
+++ Assignments/assign_07.js
@@ -12,6 +12,8 @@ const users = [
   },
 ];
 
+app.use(express.json());
+
 app.get("/", function (req, res) {
   const johnkidneys = users[0].kidneys;
   const numberofkidneys = johnkidneys.length;
@@ -32,7 +34,45 @@ app.get("/", function (req, res) {
   });
 });
 
+app.post("/", function (req, res) {
+  const isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+  res.json({
+    msg: "Done!",
+  });
+});
+
+app.put("/", function (req, res) {
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[1].healthy = true;
+  }
+  res.json({});
+});
+
+// removing all unhealthy kidneys
+app.delete("/", function (req, res) {
+  if (ThereAtleastOneUnhealthyKidney()) {
+    const newkidneys = [];
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+      if (users[0].kidneys[i].healthy) {
+        newkidneys.push({
+          healthy: true,
+        });
+      }
+    }
+    users[0].kidneys = newkidneys;
+    res.json({ msg: "done" });
+  } else {
+    res.status(411).json({
+      msg: "You have no kidneys",
+    });
+  }
+});
+
 app.listen(3002);
+
 
 // print on browser http://localhost:3002
 // output -----
