25e932063acff833ac304b13dbef69167336e61b
Vikash Kumawat <vvikash0012@gmail.com>
2025-09-07 23:54:08 +0530

    Js

---
+++ Code With harry Js/video 115/src/components/User.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const User = () => {
+  const { username } = useParams();
+  return <div>I am user {username}</div>;
+};
+
+export default User;
